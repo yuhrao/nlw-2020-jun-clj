d2c54dc4bc59421ef162ae812ef1bf8f83155d59
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-05 19:44:39 -0300

    Add server config

--- backend/src/nlw/server.clj
+++ backend/src/nlw/server.clj
@@ -12,6 +12,32 @@
 
 (def app-routes (route-table/table-routes #{["/hello" :get hello]}))
 (def url-for (route/url-for-routes app-routes))
+
+(defn stop-server! []
+  (swap! *server* http/stop))
+
+(defn add-dev-config [m]
+  (-> m
+      (http/dev-interceptors)
+      (assoc ::http/join? false)))
+
+(defn add-prod-config [m]
+  (-> m
+      (assoc ::http/join? true)
+      (assoc ::http/host "0.0.0.0")
+      (assoc ::http/port 80)
+      (assoc ::http/allowed-origins (cors/allow-origin {:creds true :allowed-origins (constantly true)}))))
+
+(defn map->server
+  ([m] (map->server m :dev))
+  ([m profile]
+   (let [base-config (-> m
+                         (http/default-interceptors))
+         full-config (cond-> base-config
+                       (= profile :dev)  (add-dev-config)
+                       (= profile :prod) (add-prod-config))]
+     (http/create-server full-config))))
+
 (defn -main [& _]
   (doto "Hello World!"
     prn))
