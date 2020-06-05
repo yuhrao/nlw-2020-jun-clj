ace34b325493e8423edf28e5f87a2e4d9fe20563
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-05 19:42:56 -0300

    Add routes

--- backend/src/nlw/server.clj
+++ backend/src/nlw/server.clj
@@ -1,5 +1,17 @@
-(ns nlw.server)
+(ns nlw.server
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.route.definition.table :as route-table]
+            [ring.util.response :as ring-res]
+            [io.pedestal.http.cors :as cors]))
 
-(defn -main [& args]
+(def hello
+  {:name ::hello
+   :enter (fn [context]
+            (assoc context :responde (ring-res/response "Hello World")))})
+
+(def app-routes (route-table/table-routes #{["/hello" :get hello]}))
+(def url-for (route/url-for-routes app-routes))
+(defn -main [& _]
   (doto "Hello World!"
     prn))
