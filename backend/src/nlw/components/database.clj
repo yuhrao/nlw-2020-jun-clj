4c7e0ea0eb1065874b87cf0a297acf2522f78f11
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-09 19:33:06 -0300

    Create create init function for database

--- backend/src/nlw/components/database.clj
+++ backend/src/nlw/components/database.clj
@@ -1,42 +1,21 @@
 (ns nlw.components.database
   (:require [next.jdbc :as jdbc]
-            [honeysql.core :as sql]
-            [honeysql-postgres.helpers :as pg-helper]
-            [honeysql-postgres.format]))
+            [integrant.core :as ig]))
 
-(def db-spec {:dbtype   "postgresql"
-              :dbname   "postgres"
-              :host     "localhost"
-              :port     5432
-              :user     "nlw"
-              :password "next_level"})
+(defmethod ig/init-key ::postgres [_ {:keys [config migrate]}]
+  (let [data-source (jdbc/get-datasource config)
+        connection  (jdbc/get-connection config)]
 
-(def data-source (jdbc/get-datasource db-spec))
+    (migrate data-source)
+    {:data-source data-source
+     :conn        connection}))
 
 (comment
-  (-> (pg-helper/create-table :nlw.t-collecttable-item)
-      (pg-helper/with-columns [[:id :uuid (sql/call :primary-key)]
-                               [:title :varchar :not :null]
-                               [:image :varchar :not :empty]])
-      #_sql/format
-      #_((partial jdbc/execute! data-source)))
+  (def db-spec {:dbtype   "postgresql"
+                :dbname   "postgres"
+                :host     "localhost"
+                :port     5432
+                :user     "nlw"
+                :password "next_level"})
 
-  (sql/format {:create-schema [:nlw]})
-  (sql/format {:drop-schema [:nlw]})
-
-  (pg-helper/drop-table :nlw.t-collect-point)
-
-  (with-open [ds (jdbc/get-datasource db-spec)]
-    (jdbc/execute! ds "SELECT * from pg_tables"))
-
-  (jdbc/execute! data-source ["CREATE TABLE t_point (id varchar(200) CONSTRAINT firstkey PRIMARY KEY)"])
-
-  (jdbc/execute! data-source (sql/format {:select [:*]
-                                          :from   [:pg-tables]
-                                          :where  [:= :tablename "nwl_migrations"]}))
-  (jdbc/execute!
-   data-source
-   ["CREATE SCHEMA nlw"])
-
-
-  )
+  (def data-source (jdbc/get-datasource db-spec)))
