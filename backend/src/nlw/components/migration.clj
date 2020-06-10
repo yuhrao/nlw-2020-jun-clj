7e7d4bd14a9d73b8536206a7d19d1af00ccf2976
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-10 11:10:21 -0300

    Create Migration component

--- backend/src/nlw/components/migration.clj
+++ backend/src/nlw/components/migration.clj
@@ -1,3 +1,8 @@
 (ns nlw.components.migration
-   )
-()
+  (:require [integrant.core :as ig]
+            [migrations.migrate :as migrations]))
+
+(defmethod ig/init-key ::migration [_ {:keys [path]}]
+  (let [migrations (migrations/get-migration-files path)]
+    (fn [data-source]
+      (migrations/up! data-source migrations))))
