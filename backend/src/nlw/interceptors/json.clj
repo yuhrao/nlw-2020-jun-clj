e5512b7076e971a6ddc2890883a245577c40a56e
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-11 10:32:53 -0300

    Refact JSON interceptor

--- backend/src/nlw/interceptors/json.clj
+++ backend/src/nlw/interceptors/json.clj
@@ -1,16 +1,15 @@
 (ns nlw.interceptors.json
   (:require [cheshire.core :as json]
-            [io.pedestal.interceptor :as interceptor]))
+            ))
 
 (def json-interception
-  (interceptor/map->Interceptor
-   {:name  ::json
-    :enter (fn [{:keys [request] :as context}]
-             (let [{method :request-method
-                    body   :body} (select-keys request [:request-method :body])]
-               (assoc-in context [:request :body]
-                         (cond-> body
-                           true slurp
-                           (contains? #{:put :post} method) (json/parse-string true)))))
-    :leave (fn [context]
-             (update-in context [:response :body] #(json/encode % true)))}))
+  {:name  ::json
+   :enter (fn [{:keys [request] :as context}]
+            (let [{method :request-method
+                   body   :body} (select-keys request [:request-method :body])]
+              (assoc-in context [:request :body]
+                        (cond-> body
+                          true                             slurp
+                          (contains? #{:put :post} method) (json/parse-string true)))))
+   :leave (fn [context]
+            (update-in context [:response :body] #(json/encode % true)))})
