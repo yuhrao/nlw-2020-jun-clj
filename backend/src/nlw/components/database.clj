171f1b6bb06054778188b3ac48d7129290fa6bce
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-11 09:58:02 -0300

    Refact database component

--- backend/src/nlw/components/database.clj
+++ backend/src/nlw/components/database.clj
@@ -5,15 +5,4 @@
 (defmethod ig/init-key ::postgres [_ {:keys [config migration]}]
   (let [data-source (jdbc/get-datasource config)]
     (migration data-source)
-    {:data-source data-source
-     :db-spec config}))
-
-(comment
-  (def db-spec {:dbtype   "postgresql"
-                :dbname   "postgres"
-                :host     "localhost"
-                :port     5432
-                :user     "nlw"
-                :password "next_level"})
-
-  (def data-source (jdbc/get-datasource db-spec)))
+    data-source))
