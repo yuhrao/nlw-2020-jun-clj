8a11bd073b09a6424942021fd3e3198f88880f24
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-11 15:37:24 -0300

    Remove old user namespace operations

--- backend/dev/user.clj
+++ backend/dev/user.clj
@@ -1,12 +1,1 @@
-(ns user
-  (:require [nlw.server :as server]))
-
-(defn start
-  ([] (start :dev))
-  ([profile]
-   (server/start-server! profile)))
-
-(defn stop
-  ([] (stop :dev))
-  ([profile]
-   (server/stop-server! profile)))
+(ns user)
