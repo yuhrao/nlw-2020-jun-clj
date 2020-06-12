b0a0e75d4b4eaaea770d6c21ca01f3d937c99d57
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-12 20:15:41 -0300

    Post point route

---
+++ backend/src/nlw/controllers/point.clj
@@ -0,0 +1,21 @@
+(ns nlw.controllers.point
+  (:require [nlw.statements.point :as point-stmt]
+            [utils.uuid :as uuid]
+            [nlw.response :as res]))
+
+(def point->uuid (uuid/map->uuid-fn :email :name))
+
+(def post-point-interceptor
+  {:name ::post
+   :enter (fn [{:keys [request postgres] :as context}]
+            (let [{:keys [body]} request
+                  point-id (point->uuid body)
+                  entity (assoc body :id point-id)]
+              (try
+                (point-stmt/save! postgres entity)
+                (assoc context :response (res/->ok entity))
+                (catch Throwable t
+                  (println t)
+                  (assoc context :response (res/->internal-server-error))))))})
+
+(def routes #{["/point" :post post-point-interceptor]})
---
+++ backend/src/nlw/statements/point.clj
@@ -0,0 +1,15 @@
+(ns nlw.statements.point
+  (:require [honeysql.core :as sql]
+            [honeysql.format]
+            [honeysql.helpers :as sql-helper]
+            [honeysql-postgres.helpers :as pg-helper]
+            [next.jdbc :as jdbc]))
+
+(def ^:private table-name :nlw.t-collect-point)
+
+(defn save! [data-source & entities]
+  (let [execute! (partial jdbc/execute! data-source)]
+    (-> (sql-helper/insert-into table-name)
+        (sql-helper/values entities)
+        sql/format
+        execute!)))
