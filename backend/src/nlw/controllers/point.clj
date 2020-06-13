e2f83f878567f61d9d065571e59653f642d6f80e
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-12 22:20:17 -0300

    Add Get by id route

--- backend/src/nlw/controllers/point.clj
+++ backend/src/nlw/controllers/point.clj
@@ -28,5 +28,23 @@
               (catch Throwable _
                 (assoc context :response (res/->internal-server-error)))))})
 
-(def routes #{["/points" :post save]
-              ["/points" :get get-all]})
+(def by-id
+  {:name ::by-id
+   :enter (fn [{:keys [request postgres] :as context}]
+            (try
+              (let [{{:keys [id]} :path-params}  request
+                    entity (point-stmt/fetch-by-id postgres (uuid/from-str id))]
+
+                (if  entity
+                  (-> context
+                      (assoc  :response (res/->ok entity)))
+                  (-> (-> context
+                      (assoc  :response (res/->not-found))))))
+              (catch IllegalArgumentException _
+                (assoc context :response (res/->bad-request)))
+              (catch Throwable _
+                (assoc context :response (res/->internal-server-error)))))})
+
+(def routes #{["/points" :get get-all]
+              ["/points/:id" :get by-id]
+              ["/points" :post save]})
