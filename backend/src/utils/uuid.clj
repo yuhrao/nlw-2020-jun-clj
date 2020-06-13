9f95bbd37f047f6163362242f9231864f499e0d8
Yuhri Bernardes <yuhri.graziano@gmail.com>
2020-06-12 22:15:57 -0300

    Reformat uuid namespace

--- backend/src/utils/uuid.clj
+++ backend/src/utils/uuid.clj
@@ -1,13 +1,15 @@
 (ns utils.uuid
   (:import java.util.UUID))
 
-(defn map->uuid-fn [& ks]
-  (fn [m]
-    (-> m
+(defn from-map [ks m]
+  (-> m
       (select-keys ks)
       sort
       vals
       set
       ((partial apply str))
       .getBytes
-      (UUID/nameUUIDFromBytes))))
+      (UUID/nameUUIDFromBytes)))
+
+(defn from-str [s]
+  (UUID/fromString s))
